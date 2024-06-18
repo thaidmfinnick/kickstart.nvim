f1daf3ddeb44011c69e9d550c5f4cc13703cbcdd
thaidmfinnick <thai.dm279@gmail.com>
2024-06-18 15:27:34 +0700

    improve: add flutter configuration

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-vim.opt.endoffile = false
+vim.opt.fixeol = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -875,6 +875,29 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
     },
     config = true,
+    opts = {
+      dev_log = {
+        enabled = true,
+        open_cmd = 'tabedit',
+      },
+      lsp = {
+        color = { -- show the derived colours for dart variables
+          enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+          background = false, -- highlight the background
+          background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+          foreground = false, -- highlight the foreground
+          virtual_text = true, -- show the highlight using virtual text
+          virtual_text_str = '■', -- the virtual text character to highlight
+        },
+        settings = {
+          showTodos = true,
+          completeFunctionCalls = true,
+          renameFilesWithClasses = 'prompt', -- "always"
+          enableSnippets = true,
+          updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+        },
+      },
+    },
   },
   {
     'kdheepak/lazygit.nvim',
